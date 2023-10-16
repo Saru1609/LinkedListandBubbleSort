60a504ddbbf3e95eac2cf9f9c7f2ef5fe9e9fa08
Saru1609 <105793129+Saru1609@users.noreply.github.com>
2023-10-16 09:19:15 +0000

    kk

--- Linkedlist.py
+++ Linkedlist.py
@@ -90,9 +90,8 @@ if __name__ == '__main__':
   ll = LinkedList()
   ll.insert_values(["banana","mango","grapes","orange"])
   ll.insert_at(1,"blueberry")
-  ll.remove_at(2)
-  ll.print()
+  
 
   ll.insert_values([45,7,12,567,99])
   ll.insert_at_end(67)
-  ll.print()
+  ll.print//09
